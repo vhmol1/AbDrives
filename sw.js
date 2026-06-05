52b8fa381b573d0c0b42d75339471d06caff0388
vhmol1 <vitorhugomol@gmail.com>
2026-06-05 12:55:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rockwell-faults-v7.15'; // Alterado para v3 para limpar o histórico do telemóvel
+const CACHE_NAME = 'rockwell-faults-v7.16'; // Alterado para v3 para limpar o histórico do telemóvel
 const ASSETS = [
   './',
   './index.html',
